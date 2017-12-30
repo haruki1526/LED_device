83ea54ca3ede3ab788b01d773ca6b9b806263300
Haruki Takamura <s1526070sa@s.chibakoudai.jp>
2017-12-31 03:09:02 +0900

    update

--- LED_device_driver.c
+++ LED_device_driver.c
@@ -1,3 +1,27 @@
+/*
+ **
+ ** LED_device_driver.c
+ ** 
+ **
+ ** Copyright (C) 2017 Haruki Takamura <s1526070sa@s.chibakoudai.jp>
+ ** 
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** (at your option) any later version.
+ ** 
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+ ** 
+ ** You should have received a copy of the GNU General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ **/
+
+
+
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
@@ -14,7 +38,7 @@ MODULE_VERSION("0.1");
 
 
 
-static volatile u32 *gpio_base =NULL; //gpioのアドレスを入れる　volatileはアドレスを変えられないように固定する
+static volatile u32 *gpio_base =NULL;
 static dev_t dev;
 static struct cdev cdv;
 
